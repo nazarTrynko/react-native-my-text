c6a1e574bf232b0c85d06b0b7c3e56cf4b3e9d62
Nazar Trynko <nazartrynko@gmail.com>
2017-09-28 18:21:00 +0300

    Add test text module

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNMyText } = NativeModules;
+
+export default RNMyText;
---
+++ ios/RNMyText.h
@@ -0,0 +1,5 @@
+#import "RCTViewManager.h"
+
+@interface RNMyText : RCTViewManager
+
+@end
